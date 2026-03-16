eecf4028f2879c8a5a5e1b7c3093019dc63aa4da
Dev Vrma <devvrma25@gmail.com>
2026-03-16 17:32:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 #1  AWS connection setup 
 
  provider "aws" { 
-  region = "ap-southeast-2"
+  region = "ap-southeast-1"
 } 
 
 #2 EC2 instance detail 
 
 resource "aws_instance" "ec2_instance" { 
-ami  = "ami-04f5097609382b1bb"
+ami  = "ami-0f58b397bc5c1f2e8"
 
 instance_type = "t3.micro"   
 
